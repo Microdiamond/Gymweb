cce28e62938ab256d030002d9191b628e2d92456
Microdiamond <chatchai.nak1997@gmail.com>
2023-04-25 08:50:07 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home/>
-    </div>
+    <Router>
+
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
